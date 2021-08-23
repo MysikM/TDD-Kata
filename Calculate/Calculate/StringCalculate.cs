3bb089542399bcba7305600428c3a17cc3a33d49
Name <Name@cbs.com>
2021-08-23 16:49:40 +0300

    Refactoring

--- Calculate/Calculate/StringCalculate.cs
+++ Calculate/Calculate/StringCalculate.cs
@@ -1,33 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Calculate
 {
     public class StringCalculate
     {
-        private int total = 0;
-        private List<string> delimetr;
+        private int result;
+       // private List<string> delimetr;
         List<int> negativeValue = new List<int>();
         private const int IGNORED_BIGGEST_NUMBER = 1000;
-        protected int Total
-        {
-            get { return total; }
-            set { total = value; }
-        }
-        public StringCalculate()
-        {
-        }
 
         public int Add(string inputData)
         {
-            delimetr = GetDelimetr(inputData);
-            string[] terms = inputData.Split(delimetr.ToArray(), StringSplitOptions.None);
-            GetSum(terms);
-            NegativeNumbersCheck();
             EmptyStringCheck(inputData);
+            //delimetr = GetDelimetr(inputData);
+            int[] numberArray = GetArray(inputData);
+            GetSum(numberArray);
+            NegativeNumbersCheck();
 
-            return Total;
+            return result;
         }
+
+        private void GetSum(int[] numberArray)
+        {
+            foreach (var numb in numberArray)
+            {
+                if (numb < 0)
+                {
+                    negativeValue.Add(numb);
+                }
+                else if (numb < IGNORED_BIGGEST_NUMBER)
+                {
+                    result += numb;
+                }
+            }
+        }
+
+        private void EmptyStringCheck(string inputData)
+        {
+            if (inputData == "")
+                result = 0;
+        }
+
+        //private static List<string> GetDelimetr(string inputData)
+        //{
+        //    var listDelimetr = new List<string> { ",", "\n" };
+        //    if (inputData.StartsWith("//["))
+        //    {
+        //        GetAllowMultipleDelimiters(inputData, listDelimetr);
+        //    }
+        //    else if (inputData.StartsWith("//"))
+        //    {
+        //        listDelimetr.Add(inputData[2].ToString());
+        //    }
+        //    return listDelimetr;
+        //}
+
+        //private static void GetAllowMultipleDelimiters(string inputData, List<string> listDelimetr)
+        //{
+        //    int startPositionDelimetr = 0;
+        //    do
+        //    {
+        //        startPositionDelimetr = inputData.IndexOf('[', startPositionDelimetr);
+
+        //        if (CheckNextDelimetr(startPositionDelimetr))
+        //            break;
+
+        //        var endPositionDelimetr = inputData.IndexOf(']', startPositionDelimetr);
+
+        //        var delimetr = inputData.Substring(startPositionDelimetr, endPositionDelimetr - startPositionDelimetr).TrimStart('[').TrimEnd(']');
+
+        //        startPositionDelimetr = endPositionDelimetr;
+
+        //        listDelimetr.Add(delimetr);
+        //    } while (true);
+        //}
+        //private static bool CheckNextDelimetr(int startPositionNextDelimetr)
+        //{
+        //    if (startPositionNextDelimetr == -1)
+        //        return true;
+        //    return false;
+        //}
+        private int[] GetArray(string inputData)
+        {
+            var numbers = Regex.Matches(inputData, @"\d+");
+            List<int> numberList = new List<int>();
+            foreach (var elem in numbers)
+            {
+                numberList.Add(int.Parse(elem.ToString()));
+            }
+
+            return numberList.ToArray();
+        }
+        
 
         private void NegativeNumbersCheck()
         {
@@ -35,91 +101,6 @@ namespace Calculate
             {
                 throw new Exception($"negatives not allowed: {String.Join(", ", negativeValue.ToArray())}");
             }
-        }
-
-        private static List<string> GetDelimetr(string inputData)
-        {
-            var delimetr = new List<string> { ",", "\n" };
-            var nextDelimetr = "";
-            var startPositionDelimetr = 0;
-            var endPositionDelimetr = 0;
-            var customDelimetr = "";
-            if (inputData.StartsWith("//["))
-            {
-                do
-                {
-                    startPositionDelimetr = inputData.IndexOf('[', startPositionDelimetr);
-                    if (startPositionDelimetr < 0)
-                    {
-                        break;
-                    }
-                    endPositionDelimetr = inputData.IndexOf(']', startPositionDelimetr);
-                    if(endPositionDelimetr < 0)
-                    {
-                        break;
-                    }
-
-                    if(endPositionDelimetr - startPositionDelimetr> 0)
-                    {
-                        nextDelimetr = inputData
-                        .Substring(startPositionDelimetr, endPositionDelimetr - startPositionDelimetr);
-                        customDelimetr = nextDelimetr
-                            .TrimStart('[')
-                            .TrimEnd(']');
-                    }
-                    else
-                    {
-                        break;
-                    }
-
-                    startPositionDelimetr = endPositionDelimetr;
-
-                    delimetr.Add(customDelimetr);
-
-                }
-                while (nextDelimetr != "");
-                
-
-            }
-            else if (inputData.StartsWith("//"))
-            {
-                delimetr.Add(inputData[2].ToString());
-            }
-            
-
-            return delimetr;
-        }
-
-        private void GetSum(string[] terms)
-        {
-            
-            foreach (var item in terms)
-            {
-
-                if (int.TryParse(item, out int numb))
-                {
-                    if(numb < 0)
-                    {
-                        negativeValue.Add(numb);
-                    }
-                    else if(numb >= IGNORED_BIGGEST_NUMBER)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        Total += numb;
-                    }
-
-                }
-            }
-           
-        }
-
-        private void EmptyStringCheck(string inputData)
-        {
-            if (inputData == "")
-                Total = 0;
         }
     }
 }
--- Calculate/CalculateTests/StringCalculateTest.cs
+++ Calculate/CalculateTests/StringCalculateTest.cs
@@ -14,93 +14,61 @@ namespace CalculateTests
         }
 
         [TestCase("",0)]
-        public void Input_EmptyString_0returned(string inputData, int expected)
+        public void Add_EmptyString_returns_0(string inputData, int expected)
         {
-
-            Assert.AreEqual(calculate.Add(inputData), expected);
-            
+            Assert.AreEqual(calculate.Add(inputData), expected);   
         }
 
         [TestCase("1",1)]
-        [TestCase("2",2)]
-        public void Input_OneNumberInString_returnedParse(string inputData, int expected)
+        public void Add_1_returns_0(string inputData, int expected)
         {
             Assert.AreEqual(calculate.Add(inputData), expected);
-
         }
 
-
-        [TestCase("1,2",3)]
         [TestCase("3,5",8)]
-        public void Input_TwoNumberInString__returnedSum(string inputData, int expected)
+        public void Add_3and5_returns_8(string inputData, int expected)
         {
-
             Assert.AreEqual(calculate.Add(inputData), expected);
-
         }
 
 
-        [TestCase("3,5,7", 15)]
-        [TestCase("3,5,7,2", 17)]
         [TestCase("3,5,7,1,12,8", 36)]
-        public void Input_UnknownNumberAmount_returnedSum(string inputData, int expected)
+        public void Add_3and5and7and1and12and8_returns_36(string inputData, int expected)
         {
-
             Assert.AreEqual(calculate.Add(inputData), expected);
-
         }
 
-
-        [TestCase("1\n2,3", 6)]
-        [TestCase("12\n2,3", 17)]
-        [TestCase("12\n2\n3", 17)]
-        [TestCase("12\n2\n3,1", 18)]
         [TestCase("10\n10\n10,10\n10", 50)]
-        public void Input_UnknownNumberAmount_WithNewLines_returnedSum(string inputData, int expected)
+        public void Add_10and10and10and10and10_returns_50(string inputData, int expected)
         {
-
             Assert.AreEqual(calculate.Add(inputData), expected);
-
         }
+
         [TestCase("//;\n1;2", 3)]
-        [TestCase("//}\n1}22}2}12", 37)]
-        [TestCase("//'\n100'10'1", 111)]
-        public void Input_UnknownNumberAmount_2_WithDifferentDelimiters_DelimiterandNewLine1andDelimiterand2_3returned(string inputData, int expected)
-        {
-
-            Assert.AreEqual(calculate.Add(inputData), expected);
-
-        }
-        [TestCase("2,1000", 2)]
-        [TestCase("2,1000,3", 5)]
-        [TestCase("//'\n1000'10'1", 11)]
-        [TestCase("10\n1000\n10,10\n10", 40)]
-
-        public void Input_NumberBiggest1000_returnedSumignoredNumberNiggest1000(string inputData, int expected)
+        public void Add_1and2WithDifferentDelimiters_returns_3(string inputData, int expected)
         {
             Assert.AreEqual(calculate.Add(inputData), expected);
-
         }
-        [TestCase("//[***]\n1***2***3", 6)]
-        public void Input_UnknownNumberAmount_WithDifferentDelimitersAnySize_returnedSum(string inputData, int expected)
+
+
+        [TestCase("10\n1000", 10)]
+        public void Add_10and1000_returns_10(string inputData, int expected)
         {
-
             Assert.AreEqual(calculate.Add(inputData), expected);
-
         }
+
+        [TestCase("//[***]\n1***900", 901)]
+
+        public void Add_1and900WithDifferentDelimitersAnySize_returns_901(string inputData, int expected)
+        {
+            Assert.AreEqual(calculate.Add(inputData), expected);
+        }
+
         [TestCase("//[***][%]\n1***2%3", 6)]
-        [TestCase("//[***][%]\n1***2%3***2", 8)]
-        [TestCase("//[;;][**]\n1**2;;3**2;;1000", 8)]
-
-
-        public void Input_UnknownNumberAmount_WithAnyDifferentDelimitersAnySize_returnedSum(string inputData, int expected)
+        public void Add_1and2and3WithAnyDifferentDelimitersAnySize_returns_6(string inputData, int expected)
         {
-
             Assert.AreEqual(calculate.Add(inputData), expected);
-
         }
-
-
 
     }
 }
