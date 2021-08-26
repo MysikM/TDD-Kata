0da2615a4858ec5aa494246477ce5bc71b3daf60
Name <Name@cbs.com>
2021-08-26 16:43:38 +0300

    Refactoring negative values check method

--- Calculate/Calculate/StringCalculate.cs
+++ Calculate/Calculate/StringCalculate.cs
@@ -7,34 +7,18 @@ namespace Calculate
     public class StringCalculate
     {
         private int result;
-       // private List<string> delimetr;
-        List<int> negativeValue = new List<int>();
         private const int IGNORED_BIGGEST_NUMBER = 1000;
 
         public int Add(string inputData)
         {
             EmptyStringCheck(inputData);
-            //delimetr = GetDelimetr(inputData);
             int[] numberArray = GetArray(inputData);
+
+            NegativeNumbersCheck(inputData);
+
             GetSum(numberArray);
-            NegativeNumbersCheck();
 
             return result;
-        }
-
-        private void GetSum(int[] numberArray)
-        {
-            foreach (var numb in numberArray)
-            {
-                if (numb < 0)
-                {
-                    negativeValue.Add(numb);
-                }
-                else if (numb < IGNORED_BIGGEST_NUMBER)
-                {
-                    result += numb;
-                }
-            }
         }
 
         private void EmptyStringCheck(string inputData)
@@ -43,45 +27,6 @@ namespace Calculate
                 result = 0;
         }
 
-        //private static List<string> GetDelimetr(string inputData)
-        //{
-        //    var listDelimetr = new List<string> { ",", "\n" };
-        //    if (inputData.StartsWith("//["))
-        //    {
-        //        GetAllowMultipleDelimiters(inputData, listDelimetr);
-        //    }
-        //    else if (inputData.StartsWith("//"))
-        //    {
-        //        listDelimetr.Add(inputData[2].ToString());
-        //    }
-        //    return listDelimetr;
-        //}
-
-        //private static void GetAllowMultipleDelimiters(string inputData, List<string> listDelimetr)
-        //{
-        //    int startPositionDelimetr = 0;
-        //    do
-        //    {
-        //        startPositionDelimetr = inputData.IndexOf('[', startPositionDelimetr);
-
-        //        if (CheckNextDelimetr(startPositionDelimetr))
-        //            break;
-
-        //        var endPositionDelimetr = inputData.IndexOf(']', startPositionDelimetr);
-
-        //        var delimetr = inputData.Substring(startPositionDelimetr, endPositionDelimetr - startPositionDelimetr).TrimStart('[').TrimEnd(']');
-
-        //        startPositionDelimetr = endPositionDelimetr;
-
-        //        listDelimetr.Add(delimetr);
-        //    } while (true);
-        //}
-        //private static bool CheckNextDelimetr(int startPositionNextDelimetr)
-        //{
-        //    if (startPositionNextDelimetr == -1)
-        //        return true;
-        //    return false;
-        //}
         private int[] GetArray(string inputData)
         {
             var numbers = Regex.Matches(inputData, @"\d+");
@@ -93,14 +38,38 @@ namespace Calculate
 
             return numberList.ToArray();
         }
-        
 
-        private void NegativeNumbersCheck()
+        private static void NegativeNumbersCheck(string inputData)
         {
-            if (negativeValue.Count > 0)
+            List<int> negativeValues = new List<int>();
+
+            var negativeNumbers = Regex.Matches(inputData, @"-\d+");
+            foreach (var elem in negativeNumbers)
             {
-                throw new Exception($"negatives not allowed: {String.Join(", ", negativeValue.ToArray())}");
+                negativeValues.Add(int.Parse(elem.ToString()));
+            }
+
+            if (negativeValues.Count > 0)
+            {
+                throw new Exception($"negatives not allowed: {String.Join(", ", negativeValues.ToArray())}");
             }
         }
+
+        private void GetSum(int[] numberArray)
+        {
+            foreach (var numb in numberArray)
+            {
+                if (numb < IGNORED_BIGGEST_NUMBER)
+                {
+                    result += numb;
+                }
+            }
+        }
+
+
+
+
+        
+
     }
 }
